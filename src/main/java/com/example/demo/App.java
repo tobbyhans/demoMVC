4ac264305a99b9f5df8f3d9ddaa7ff912375150a
Danilo <Danilo@TIMON>
2018-05-30 00:53:07 +0200

    commit

---
+++ src/main/java/com/example/demo/App.java
@@ -0,0 +1,73 @@
+package com.example.demo;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class App {
+
+	@RequestMapping(value = "/saludoinicial")
+	@ResponseBody
+	public String saludoInicial() {
+		return "saludoInicial";
+
+	}
+
+	@RequestMapping(value = "/people", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public String getPeople() {
+		return "juan";
+	}
+
+	@RequestMapping(value = "/test")
+	@ResponseBody
+	public Persona getpersona1() {
+		return new Persona("pepe", "juan");
+	}
+	
+
+//THIS IS WICH QUERY-STRING (?) , @RequestParam String parametro
+
+	@RequestMapping(value = "/conpatrametro", method = RequestMethod.GET)
+	@ResponseBody
+	public String conParametro(@RequestParam String parametro) {
+
+		return "valor retornado con parametro  ::: " + parametro;
+	}
+	
+	
+	@RequestMapping(value = "/pathvariable/{variable}", method = RequestMethod.GET)
+	@ResponseBody
+	public String conVariable(@PathVariable String variable) {
+
+		return "valor retornado con variable   :::  " + variable;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/example/demo/Persona.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+public class Persona {
+	
+
+		String nombre;
+
+		
+
+		public Persona() {}
+
+		public Persona(String string) {
+			// TODO Auto-generated constructor stub
+		}
+
+		public Persona(String string, String string2) {
+			// TODO Auto-generated constructor stub
+		}
+
+		public String getNombre() {
+			return nombre;
+		}
+
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+
+		@Override
+		public String toString() {
+			return "Persona [nombre=" + nombre + "]";
+		}
+		
+	
+
+	
+
+}
